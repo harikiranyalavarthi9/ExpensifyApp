a44c41b7455777c784db2f04b2fe68e055c8adee
Hari Kiran Yalavarthi <harikiranyalavarthi@qasymphony.com>
2018-12-18 02:05:33 -0500

    Organized the app by created file for each component

---
+++ src/components/ExpensifyDashboardPage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const ExpensifyDashboardPage = () => (
+    <div>
+        This is from my dashboard component
+    </div>
+);
+
+export default ExpensifyDashboardPage;
