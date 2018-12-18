a411c17c0eafc8127a05f806039d46cc8efae126
Hari Kiran Yalavarthi <harikiranyalavarthi@qasymphony.com>
2018-12-18 11:09:41 -0500

    Implemented subscribe, unsubscribe and dynamic actions

--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -1,21 +1,27 @@
 import { createStore } from 'redux';
-import { setPriority } from 'os';
 
 const store = createStore((state = { count: 0 }, action) => {
     switch(action.type) {
         case 'INCREMENT':
+            const incrementBy = typeof action.incrementBy === 'number' ? action.incrementBy : 1;
             return {
-                count: state.count + 1
+                count: state.count + incrementBy
             };
         
         case 'DECREMENT':
+            const decrementBy = typeof action.decrementBy === 'number' ? action.decrementBy : 1;
             return {
-                count: state.count - 1
+                count: state.count - decrementBy
             }
         
         case 'RESET':
             return {
                 count: 0
+            }
+        
+        case 'SET':
+            return {
+                count: action.count
             }
             
         default:
@@ -23,16 +29,36 @@ const store = createStore((state = { count: 0 }, action) => {
     }
 });
 
-console.log(store.getState());
-store.dispatch({
-    type: 'INCREMENT'
+const unsubscribe = store.subscribe(() => {
+    console.log(store.getState());
 });
-console.log(store.getState());
+
+
+store.dispatch({
+    type: 'INCREMENT',
+});
+
 store.dispatch({
     type: 'RESET'
 });
-console.log(store.getState());
+
 store.dispatch({
-    type: 'DECREMENT'
+    type: 'INCREMENT',
+    incrementBy : 5
 });
-console.log(store.getState());
+
+// unsubscribe();
+
+store.dispatch({
+    type: 'DECREMENT',
+});
+
+store.dispatch({
+    type: 'DECREMENT',
+    decrementBy: 10
+});
+
+store.dispatch({
+    type: 'SET',
+    count: 101
+});
